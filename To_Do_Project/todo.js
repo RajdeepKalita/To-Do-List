b4dd9de96fed17e4f3ba0361967dec683939ef2f
RajdeepKalita <56757435+RajdeepKalita@users.noreply.github.com>
2020-10-24 15:54:15 +0530

    Add files via upload

---
+++ To_Do_Project/todo.js
@@ -0,0 +1,20 @@
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+	
+	
+	
+});
+$("ul").on("click","span",function(event){
+	event.stopPropagation();
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+});
+$("input").keypress(function(event){
+	if(event.which==13){
+		var text=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash '></i> </span>"+text+"</li>");
+	}
+});
+
